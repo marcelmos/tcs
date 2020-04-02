d43496143f2ba07bed813e61d39b8339d489daf0
Draster00 <kangurexplay@gmail.com>
2020-04-02 18:32:18 +0200

    New version v1.7c. All changes in changelog.txt

--- report.php
+++ report.php
@@ -35,15 +35,12 @@ $db = mysqli_connect($host, $db_user, $db_pass, $db);
         $year = $_POST['year'];
         $month = $_POST['month'];
         $difference = 0;
-        //$lastValue = 0;
+        $differenceCounter = 0;
         $sumValue = 0;
 
         $query = mysqli_query($db, "SELECT lokatorzy.imie, lokatorzy.nazwisko, idLokatora, ROUND(stanLicznika, 2) AS stanLicznika, dataOdczytu FROM dane JOIN lokatorzy ON dane.idLokatora = lokatorzy.id WHERE  MONTH(dataOdczytu) = '$month'  and YEAR(dataOdczytu) = '$year' ORDER BY idLokatora ASC");
 
-//        $resoult = mysqli_fetch_array($query);
-
         while($resoult = mysqli_fetch_array($query)){
-//            $lastValue = array(0); //Setting var
 
             if($month == "01"){
                 $prevMonth = 12;
@@ -54,10 +51,7 @@ $db = mysqli_connect($host, $db_user, $db_pass, $db);
                 $prevYear = $year;
             }
 
-            //$queryLast = mysqli_query($db, "SELECT ROUND(stanLicznika, 2) AS stanLicznika FROM dane WHERE idLokatora = '".$resoult['idLokatora']."' AND MONTH(dataOdczytu) = '$prevMonth' and YEAR(dataOdczytu) = '$prevYear' GROUP BY dataOdczytu ASC");
             $queryLast = mysqli_query($db, "SELECT ROUND(stanLicznika, 2) AS stanLicznika FROM dane WHERE idLokatora = '".$resoult['idLokatora']."' AND MONTH(dataOdczytu) = '$prevMonth' and YEAR(dataOdczytu) = '$prevYear' GROUP BY dataOdczytu ASC");
-
-//            $queryLast = mysqli_query($db, "SELECT stanLicznika FROM dane JOIN lokatorzy ON  dane.idLokatora = lokatorzy.id WHERE lokatorzy.id = ".$resoult['id']." AND (dataOdczytu >= '$prevYearMonth-01' AND dataOdczytu <= '$prevYearMonth-31') GROUP BY dataOdczytu DESC");
 
             $lastValue = mysqli_fetch_array($queryLast);
 
@@ -71,12 +65,6 @@ $db = mysqli_connect($host, $db_user, $db_pass, $db);
                 $lastValueInt = 0;
             }
 
-//            echo "<tr><td>".$resoult['imie']." ".$resoult['nazwisko']."</td>
-//                <td>".$lastValue['stanLicznika']."</td>
-//                <td>".$resoult['stanLicznika']."m<sup>3</sup></td>
-//                <td>".$resoult['dataOdczytu']."</td>
-//                <td>".($resoult['stanLicznika'] - $lastValue['stanLicznika'])."m<sup>3</sup></td></tr>";
-
             echo "<tr><td>".$resoult['imie']." ".$resoult['nazwisko']."</td>
                 <td>".$lastValueShow."</td>
                 <td>".$resoult['stanLicznika']."m<sup>3</sup></td>
@@ -85,14 +73,51 @@ $db = mysqli_connect($host, $db_user, $db_pass, $db);
 
             $sumValue += $difference;
         }
-        echo "<tr><td colspan='3'></td> <th>Suma: </th> <td>".number_format($sumValue, 2)."m<sup>3</sup></td></tr>";
-        $mainCounter = mysqli_query($db, "SELECT stanLicznika FROM glowny_licznik WHERE MONTH(dataOdczytu) = '$month'  and YEAR(dataOdczytu) = '$year'");
-        $counterStr = mysqli_fetch_array($mainCounter);
-        echo "<tr><td colspan='3'></td> <th>Główny licznik: </th> <td>".number_format($counterStr[0], 2)."m<sup>3</sup></td></tr>";
+
+
+
+        //Main counter row
+        $queryMainCounter = mysqli_query($db, "SELECT ROUND(stanLicznika, 2) AS stanLicznika, dataOdczytu FROM glowny_licznik WHERE MONTH(dataOdczytu) = '$month'  and YEAR(dataOdczytu) = '$year'");
+
+        while($counterStr = mysqli_fetch_array($queryMainCounter)){
+
+            if($month == "01"){
+                $prevMonth = 12;
+                $prevYear = ($year-1);
+            }
+            if($month != "01"){
+                $prevMonth = ($month-1);
+                $prevYear = $year;
+            }
+
+            $queryMainCounterLast = mysqli_query($db, "SELECT ROUND(stanLicznika, 2) AS stanLicznika, dataOdczytu FROM glowny_licznik WHERE MONTH(dataOdczytu) = '$prevMonth'  and YEAR(dataOdczytu) = '$prevYear'");
+
+
+            $lastValue = mysqli_fetch_array($queryMainCounterLast);
+
+            if($lastValue > 0){
+                $lastValueInt = $lastValue[0];
+                $lastValueShow = $lastValueInt."m<sup>3</sup>";
+                $differenceCounter = ($counterStr['stanLicznika'] - $lastValue[0]);
+            }else{
+                $lastValueShow = "Brak Danych";
+                $differenceCounter = 0;
+                $lastValueInt = 0;
+            }
+
+            echo "<tr><th>Główny licznik</th>
+            <td>".$lastValueShow."</td>
+            <td>".$counterStr['stanLicznika']."m<sup>3</sup></td>
+            <td>".$counterStr['dataOdczytu']."</td>
+            <td>".number_format($differenceCounter, 2)."m<sup>3</sup></td></tr>";
+        }
+        echo "<tr><td colspan='3'></td> <th>Suma różnicy z lokatorów: </th> <td>".number_format($sumValue, 2)."m<sup>3</sup></td></tr>";
+
+        $glownyOdLokatorow = $differenceCounter - $sumValue;
+        echo "<tr><td colspan='2'></td> <th colspan='2'>Różnica głównego licznika od sumy różnicy lokatorów: </th> <td>".number_format($glownyOdLokatorow, 2)."m<sup>3</sup></td></tr>";
         mysqli_close($db);
         ?>
 
     </tr>
 </table>
-
 </html>
